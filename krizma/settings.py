9b04fa537872da5f54e0fae19a22b3f80d108f1c
RyanC10 <108695990+RyanC10@users.noreply.github.com>
2023-03-09 08:11:53 +0100

    Update settings.py

--- krizma/settings.py
+++ krizma/settings.py
@@ -120,5 +120,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_STORAGE='whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 CSRF_TRUSTED_ORIGINS = ['https://emanuel-kviz.up.railway.app']
